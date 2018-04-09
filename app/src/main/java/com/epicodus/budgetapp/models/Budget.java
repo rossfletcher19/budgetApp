0c21c585e5f6e2522c02c597a7b3dda8f8dc0969
Ross Fletcher <rtbfletch@outlook.com>
2018-04-09 16:14:21 -0700

    add budget and transaction models with initial properties

---
+++ app/src/main/java/com/epicodus/budgetapp/models/Budget.java
@@ -0,0 +1,59 @@
+package com.epicodus.budgetapp.models;
+
+import java.util.ArrayList;
+
+public class Budget {
+    private Integer net_income;
+    private Integer after_mandatory_exp;
+    private Integer running_bal;
+    private ArrayList<Transaction> transactions;
+    private String month;
+
+    public Budget(Integer net_income, Integer after_mandatory_exp, Integer running_bal, ArrayList<Transaction> transactions, String month) {
+        this.net_income = net_income;
+        this.after_mandatory_exp = after_mandatory_exp;
+        this.running_bal = running_bal;
+        this.transactions = transactions;
+        this.month = month;
+    }
+
+    public Integer getNet_income() {
+        return net_income;
+    }
+
+    public void setNet_income(Integer net_income) {
+        this.net_income = net_income;
+    }
+
+    public Integer getAfter_mandatory_exp() {
+        return after_mandatory_exp;
+    }
+
+    public void setAfter_mandatory_exp(Integer after_mandatory_exp) {
+        this.after_mandatory_exp = after_mandatory_exp;
+    }
+
+    public Integer getRunning_bal() {
+        return running_bal;
+    }
+
+    public void setRunning_bal(Integer running_bal) {
+        this.running_bal = running_bal;
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(ArrayList<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+}
---
+++ app/src/main/java/com/epicodus/budgetapp/models/Transaction.java
@@ -0,0 +1,56 @@
+package com.epicodus.budgetapp.models;
+
+public class Transaction {
+    private String date;
+    private String note;
+    private Integer amount;
+    private String transType;
+    private Integer id;
+
+    public Transaction(String date, String note, Integer amount, String transType) {
+        this.date = date;
+        this.note = note;
+        this.amount = amount;
+        this.transType = transType;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getTransType() {
+        return transType;
+    }
+
+    public void setTransType(String transType) {
+        this.transType = transType;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
